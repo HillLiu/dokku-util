929e19649e4fb4a162e170171dc777f3d53fdafe
Hill <hill@kimo.com>
2021-04-09 21:23:28 +0800

    remove folder name

--- to-dokku.sh
+++ to-dokku.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
-remote=`${DIR}/support/dokku-remote.sh`
+remote=`${DIR}/dokku-remote.sh`
 local=`git branch | grep \* | cut -d ' ' -f2`
 
 git add -A; git commit --amend --no-edit; git push ${remote} ${local}:master -f
