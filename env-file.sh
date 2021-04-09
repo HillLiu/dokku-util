4ec34f55d8de58552bda70d48b68958711b36b1b
Hill <hill@kimo.com>
2021-04-09 13:34:07 +0000

    update file

--- env-file.sh
+++ env-file.sh
@@ -2,9 +2,11 @@
 
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
+DEFAULT_ENV_FILE="${DIR}/../.env.dokku"
+
 if [ -z $ENV_FILE ]; then
-  if [ -e .env ]; then
-    ENV_FILE="${DIR}/../.env"
+  if [ -e "$DEFAULT_ENV_FILE" ]; then
+    ENV_FILE=$DEFAULT_ENV_FILE
   else
     ENV_FILE="${DIR}/../.env.sample"
   fi
