0327b4471fbc3365c4efac8fc39521f90da1ac1b
Hill <hill@kimo.com>
2021-04-09 13:34:07 +0000

    update file

--- to-dokku.sh
+++ to-dokku.sh
@@ -4,4 +4,7 @@ DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 remote=`${DIR}/dokku-remote.sh`
 local=`git branch | grep \* | cut -d ' ' -f2`
 
-git add -A; git commit --amend --no-edit; git push ${remote} ${local}:master -f
+git add -A && \
+  git commit --amend --no-edit && \
+  git push ${remote} ${local}:master -f && \
+  git reset origin/${local}
