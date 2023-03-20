cc9ee9f47647a9b6e8be438dfce24de69e741dfe
Hill <hill@kimo.com>
2023-03-20 21:12:14 +0800

    update StrictHostKeyChecking

--- to-dokku.sh
+++ to-dokku.sh
@@ -4,7 +4,9 @@ DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 remote=`${DIR}/dokku-remote.sh`
 local=`git branch | grep \* | cut -d ' ' -f2`
 
-GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git add -A && \
+export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no"
+
+git add -A && \
   git commit --amend --no-edit && \
   git push ${remote} ${local}:master -f && \
   git reset origin/${local}
